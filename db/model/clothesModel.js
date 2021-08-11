ebd28f46aa8f7ebf5d1c15b91d4fd79b97209478
Alexey <sologod@yandex.ru>
2021-08-11 09:30:18 +0300

    Models order and clothes

---
+++ db/model/clothesModel.js
@@ -0,0 +1,28 @@
+const {Schema, model} = require('mongoose');
+
+const clothesSchema = Schema ({
+  name:  {
+    type: String,
+    required: true,
+  },
+  color:  {
+    type: String,
+    required: true,
+  },
+  typeOfClothes:  {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+details:  {
+    type: String,
+    required: false,
+  },
+});
+
+const clothesModel = model('Clothes', clothesSchema);
+
+module.exports = clothesModel;
